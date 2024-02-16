45688f07d62cb93f9ef23f2c43b6f5ea7341a626
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2024-02-16 13:55:42 -0400

    feat: Sending e-mails

--- src/actions/send-email.ts
+++ src/actions/send-email.ts
@@ -1,9 +1,11 @@
+"use server";
+
 import { SendEmailDto } from "@/types/send-email.dto";
 import axios from "axios";
 
 export async function sendEmail(email: SendEmailDto) {
   const res = await axios
-    .post(`${process.env.API_URL}/sendEmail`, JSON.stringify(email), {
+    .post(`${process.env.API_URL}/sendMail`, JSON.stringify(email), {
       headers: {
         "Content-Type": "application/json",
       },
